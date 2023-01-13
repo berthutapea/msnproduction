2c083b24b4ab8421f4678e75afe30a5fea04bf1d
Gilbert Hutapea <berthutapea@gmail.com>
2023-01-14 01:22:00 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,339 @@
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  let selectTopbar = select('#topbar')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+        if (selectTopbar) {
+          selectTopbar.classList.add('topbar-scrolled')
+        }
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+        if (selectTopbar) {
+          selectTopbar.classList.remove('topbar-scrolled')
+        }
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+  * Back to WA button
+  */
+  let backtowa = select('.back-to-wa')
+  if (backtowa) {
+    const toggleBacktowa = () => {
+      if (window.scrollY > 100) {
+        backtowa.classList.add('active')
+      } else {
+        backtowa.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktowa)
+    onscroll(document, toggleBacktowa)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function (e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function (e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>" :
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+  // /**
+  //  * Jasa isotope and filter
+  //  */
+  // window.addEventListener('load', () => {
+  //   let jasaContainer = select('.jasa-container');
+  //   if (jasaContainer) {
+  //     let jasaIsotope = new Isotope(jasaContainer, {
+  //       itemSelector: '.jasa-item',
+  //       layoutMode: 'fitRows'
+  //     });
+
+  //     let jasaFilters = select('#jasa-flters li', true);
+
+  //     on('click', '#jasa-flters li', function (e) {
+  //       e.preventDefault();
+  //       jasaFilters.forEach(function (el) {
+  //         el.classList.remove('filter-active');
+  //       });
+  //       this.classList.add('filter-active');
+
+  //       jasaIsotope.arrange({
+  //         filter: this.getAttribute('data-filter')
+  //       });
+
+  //     }, true);
+  //   }
+
+  // });
+
+  window.addEventListener('load', () => {
+    let pricingContainer = select('.pricing-container');
+    if (pricingContainer) {
+      let pricingIsotope = new Isotope(pricingContainer, {
+        itemSelector: '.pricing-item',
+        layoutMode: 'fitRows'
+      });
+
+      let pricingFilters = select('#pricing-flters li', true);
+
+      on('click', '#pricing-flters li', function (e) {
+        e.preventDefault();
+        pricingFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        pricingIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
+  });
+
+  /* ..............................................
+  Countdown Clock
+  ................................................. */
+  // Set the date you're counting down to
+  var countDownDate = new Date("Jan 18, 2023 00:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    document.getElementById("days").innerHTML = days + " Hari ";
+    document.getElementById("hours").innerHTML = hours + " Jam ";
+    document.getElementById("minutes").innerHTML = minutes + " Menit ";
+    document.getElementById("seconds").innerHTML = seconds + " Detik ";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+})()
+
+// /**
+//  * Menu isotope and filter
+//  */
+// window.addEventListener('load', () => {
+//   let menuContainer = select('.menu-container');
+//   if (menuContainer) {
+//     let menuIsotope = new Isotope(menuContainer, {
+//       itemSelector: '.menu-item',
+//       layoutMode: 'fitRows'
+//     });
+
+//     let menuFilters = select('#menu-flters li', true);
+
+//     on('click', '#menu-flters li', function (e) {
+//       e.preventDefault();
+//       menuFilters.forEach(function (el) {
+//         el.classList.remove('filter-active');
+//       });
+//       this.classList.add('filter-active');
+
+//       menuIsotope.arrange({
+//         filter: this.getAttribute('data-filter')
+//       });
+//       menuIsotope.on('arrangeComplete', function () {
+//         AOS.refresh()
+//       });
+//     }, true);
+//   }
+
+// });
+
+/**
+* Animation on scroll
+*/
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  })
+});
+
+
---
+++ forms/index.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var nodemailer = require("nodemailer");
+
+var app = express();
+var server = http.Server(app);
+var port = 500;
+
+app.set("port", port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "index.html")));
+
+// Routing
+app.get("/", function (_req, response) {
+  response.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.post("/send_email", function (req, response) {
+  //variabel yang menampung desain email yang akan dikirim
+  var name = req.body.name;
+  var from = req.body.from;
+  var subject = req.body.subject;
+  var message = req.body.message;
+  var pesan = message + "\n \n Detail pengirim : " +
+    "\n Nama Lengkap : " + name +
+    "\n Alamat Email : " + from;
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'hutapeabert2002@gmail.com',
+      pass: '#Hutapea211002'
+    }
+  });
+
+  var mailOptions = {
+    from: from,
+    to: 'hutapeabert2002@gmail.com',
+    subject: subject,
+    text: pesan
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log("Email Sent: " + info, response)
+    }
+    response.redirect("/")
+  })
+})
+
+//Initialize Web Server
+server.listen(port, function () {
+  console.log("starting server on port " + port)
+})
+console.clear()
