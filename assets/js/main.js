ff65a2c4c196a14ddb93fbb4308da8b7a2b546ed
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-19 19:28:53 +0700

    Refactor Paket

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,7 +236,7 @@
   Countdown Clock
   ................................................. */
   // Set the date you're counting down to
-  var countDownDate = new Date("Mar 19, 2023 00:00:00").getTime();
+  var countDownDate = new Date("Mar 26, 2023 00:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
